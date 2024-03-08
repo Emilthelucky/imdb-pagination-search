74f7abdd286893c87f0e4fbe45202bc4946a6021
GitHub Kullanıcı Adınız <asifadisirinov@gmail.com>
2024-03-08 12:45:28 -0800

    changes

--- IMDB-API/main.js
+++ IMDB-API/main.js
@@ -14,6 +14,6 @@ dbConnect()
 //middlewares
 app.use(cors())
 app.use(express.json())
-app.use("/movies", movieRouter)
+app.use("/https://imdb-api-alpha.vercel.app/", movieRouter)
 
 app.listen(PORT, console.log("PORT 8080"))
